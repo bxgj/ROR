6b8736f1158802d695942d5b8ba700ef480b69fe
zqb <13600363246@139.com>
2016-09-18 17:40:11 +0800

    ruby on rail sample

--- ROR/blog/app/controllers/articles_controller.rb
+++ ROR/blog/app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+http_basic_authenticate_with name: "zqb", password: "zqb", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
